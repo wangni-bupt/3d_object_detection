3312205c0de211de76b8a2f61a211ebd4d9908d0
wangni-bupt <282133752@qq.com>
2021-07-30 14:45:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a3d_object_detection/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a3d_object_detection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.a3d_object_detection.databinding.ActivityMainBinding;
+
+import com.google.ar.core.Session;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
